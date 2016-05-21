563b0723f9aa254af52937a5d49fd23383460b00
Anonym Anonymov <Anonym Anonymov>
2016-05-21 22:13:50 +0300

    Finished styling of InoutTestName activity, added button action listener

--- app/src/main/java/ua/nure/shylin/cards/InputTestNameActivity.java
+++ app/src/main/java/ua/nure/shylin/cards/InputTestNameActivity.java
@@ -2,6 +2,7 @@ package ua.nure.shylin.cards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InputTestNameActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class InputTestNameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_test_name);
+    }
+
+    public void addTest(View view){
+        //TODO доделать запись в БД и передачу Id в следующую активити
     }
 }
