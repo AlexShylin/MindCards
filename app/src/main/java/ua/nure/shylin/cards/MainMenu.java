0fcc1ff8ac77133700a452cf2f1680f234a46506
Anonym Anonymov <Anonym Anonymov>
2016-05-21 18:13:24 +0300

    Completed main menu (text, links), added Start activity (+text)

--- app/src/main/java/ua/nure/shylin/cards/MainMenu.java
+++ app/src/main/java/ua/nure/shylin/cards/MainMenu.java
@@ -1,7 +1,9 @@
 package ua.nure.shylin.cards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startCards(View view){
+        // Обработка нажатия кнопки "Начать"
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
+    }
+
+    public void openInfo(View view){
+        // Обработка нажатия кнопки "Справка"
+        // TODO доделать активити справки
+    }
+
+    public void exitApp(View view) {
+        // Обработка нажатия кнопки "Выход"
+        this.finish();
     }
 }
